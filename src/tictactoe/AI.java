a63e8e221edf520c78732764ed0648c817b49edd
Tim Chen <tchen994@gmail.com>
2021-04-22 23:48:46 -0400

    Merged from Game class as the previous cpuCoordinate method

--- src/tictactoe/AI.java
+++ src/tictactoe/AI.java
@@ -8,7 +8,32 @@ public class AI {
         this.random = new Random();
     }
 
-    public int randomNumber() {
-        return random.nextInt(3) + 1;
+    public void easyCPU(Board board) {
+
+        String[][] gameBoard = board.currentBoard();
+        int row;
+        int col;
+
+        while (true) {
+            try {
+                row = random.nextInt(3) + 1;
+                col = random.nextInt(3) + 1;
+
+                if (row < 0 || col < 0 || row > 3 || col > 3) {
+                    continue;
+                }
+                if (gameBoard[row - 1][col - 1].contains("X") ||
+                        gameBoard[row - 1][col - 1].contains("O")){
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            System.out.println("Making move level \"easy\"");
+            break;
+        }
+
+        board.updateBoard(row, col);
     }
 }
